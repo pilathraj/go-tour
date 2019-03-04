92c7ed771088a023e9117ff94d894c3033009eed
pilathraj <pilathraj@gmail.com>
2019-03-04 12:02:06 +0530

    zero values

---
+++ zero-values/main.go
@@ -0,0 +1,12 @@
+// Package main print zero value for some basic type
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", i, f, b, s) // output: 0 0.0 false ""
+}
---
+++ zero-values/readme.md
@@ -0,0 +1,8 @@
+# Go's basic types are
+
+
+```shell
+cmd > go build main.go
+cmd > main
+cmd > 0 0 false ""
+```
