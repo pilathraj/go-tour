ad5107a6e64a8459cae734b112153ec202a8ccdd
pilathraj <pilathraj@gmail.com>
2019-03-04 11:28:29 +0530

    variables with initializer

---
+++ variables-with-initializers/main.go
@@ -0,0 +1,11 @@
+// Package main variable declaration with initializer
+package main
+
+import "fmt"
+
+var i, j int = 1, 2 // variable declaration at package level
+
+func main() {
+	var c, python, java = true, false, "Yes" //  variable will take the type of the initializer
+	fmt.Println(i, c, python, java)          // output:  1 true false Yes
+}
---
+++ variables-with-initializers/readme.md
@@ -0,0 +1,5 @@
+```shell
+cmd > go build main.go
+cmd > main
+cmd > 1 true false Yes // output
+```
