d7841eea46cdc1a16a6889b073da5c8c3b97a5fa
pilathraj <pilathraj@gmail.com>
2019-03-04 11:51:46 +0530

    basic types

---
+++ basic-types/main.go
@@ -0,0 +1,19 @@
+// Package main type and value
+package main
+
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+var (
+	ToBe   bool       = true
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
+func main() {
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)     // Type: bool Value: true
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt) // Type: uint64 Value: 18446744073709551615
+	fmt.Printf("Type: %T Value: %v\n", z, z)           // Type: complex128 Value: (2+3i)
+}
---
+++ basic-types/readme.md
@@ -0,0 +1,28 @@
+# Go's basic types are
+```go
+bool
+
+string
+
+int  int8  int16  int32  int64
+uint uint8 uint16 uint32 uint64 uintptr
+
+byte // alias for uint8
+
+rune // alias for int32
+     // represents a Unicode code point
+
+float32 float64
+
+complex64 complex128
+
+```
+
+
+```shell
+cmd > go build main.go
+cmd > main
+cmd > Type: bool Value: true
+Type: uint64 Value: 18446744073709551615
+Type: complex128 Value: (2+3i)
+```
