53f61dc5a489be883d63e92a554a6035bf7d0d4a
pilathraj <pilathraj@gmail.com>
2019-03-04 14:29:31 +0530

    lib function added

---
+++ for-loop/lib/sum.go
@@ -0,0 +1,11 @@
+// Package main calculate sum of 100 number
+package lib
+
+func Add() int {
+	sum := 1
+	// init and post statements are optional.
+	for sum < 1000 {
+		sum += sum
+	}
+	return sum
+}
--- for-loop/main.go
+++ for-loop/main.go
@@ -1,13 +1,18 @@
 // Package main for calculate sum of 0 to 9
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go-tour/for-loop/lib"
+)
 
 func main() {
 	sum := 0
 	for i := 0; i < 10; i++ {
 		sum += i
-		//fmt.Println(i)
+		//cd .fmt.Println(i)
 	}
 	fmt.Println("Sum: ", sum) //Sum:  45
+	sum = lib.Add()
+	fmt.Println("Sum: ", sum) //Sum:  1024
 }
